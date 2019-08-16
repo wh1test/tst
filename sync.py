1383f0a9acf3a0598260e5f648be25ed0d99e1ee
Yury Kazakevich <y.whitest@gmail.com>
2019-08-16 23:30:52 +0300

    999

--- sync.py
+++ sync.py
@@ -6,9 +6,10 @@ import re
 def git_push(PATH_OF_GIT_REPO, REMOTE):
     try:
         repo = Repo(PATH_OF_GIT_REPO)
+        REMOTE = str(REMOTE).strip()
         repo.git.add(update=True)
         #repo.git.add()
-        origin = repo.remote(name=(str(REMOTE)))
+        origin = repo.remote(name=REMOTE)
         #origin.fetch()
         origin.push(force=True)
         print('Successfully pushed to: ', REMOTE)
@@ -31,20 +32,19 @@ def readcfg(CFG):
                     res.append(match[0][2])
                 c += 1
     else:
-        print("Config not found! Change you apth to git working directory and try again. Exiting...")
+        print("Config not found! Change you path to git working directory and try again. Exiting...")
         sys.exit(1)
     return res
 
-#tst
-#temp test
-#44444
 PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = input("Specify a comment please: ")
+COMMIT_MESSAGE = str(COMMIT_MESSAGE).strip()
+
 CFG = f"{PATH_OF_GIT_REPO}/config"
-    #git_push()
+
 w = readcfg(CFG)
 repo = Repo(PATH_OF_GIT_REPO)
-repo.index.commit(str(COMMIT_MESSAGE).strip())
+repo.index.commit(COMMIT_MESSAGE)
 
 for REMOTE in w:
     if REMOTE != 'prepod':
